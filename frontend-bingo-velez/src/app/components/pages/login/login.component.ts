150e1ea1b65aa531a58952469cc6b799a9c39e4e
daniel.tavarez <daniel.tavarez@solvex.com.do>
2022-05-16 22:06:20 -0400

    Login, home and navbar

--- frontend-bingo-velez/src/app/components/pages/login/login.component.ts
+++ frontend-bingo-velez/src/app/components/pages/login/login.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  Sign: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  sign(){
+    this.Sign = !this.Sign;
+  }
 }
