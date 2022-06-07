c993ffdf46d5bd1d11403f2ade1ff01bd68dfe2e
Noe1601 <noeeduardomedinahenriquez@gmail.com>
2022-06-07 19:32:57 -0400

    Modulo de mantenimiento usuarios listo

--- frontend-bingo-velez/src/app/components/pages/pages-routing.module.ts
+++ frontend-bingo-velez/src/app/components/pages/pages-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './home/home.component';
 import { PagesComponent } from './pages.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SettingsComponent } from './settings/settings.component';
+import { UserDesactivatedComponent } from './user-desactivated/user-desactivated.component';
 import { UsersComponent } from './users/users.component';
 
 
@@ -18,6 +19,7 @@ export const routes: Routes = [
         { path: 'users', component: UsersComponent },
         { path: 'profile/:id', component: ProfileComponent },
         { path: 'settings', component: SettingsComponent },
+        {path: 'desactivatedUsers', component: UserDesactivatedComponent}
     ]
   },
 ];
