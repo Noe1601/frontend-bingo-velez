0255259dd83332d8382a4461902bd7055907cf70
daniel.tavarez <daniel.tavarez@solvex.com.do>
2022-06-26 14:53:22 -0400

    marcar numeros en cartones, generarlos y listarlos

--- frontend-bingo-velez/src/app/core/components/sidenav/sidenav.component.ts
+++ frontend-bingo-velez/src/app/core/components/sidenav/sidenav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { SharedService } from 'src/app/core/services/shared.service';
+import { HomeService } from '../../services/home.service';
 import { ValidateRoleService } from '../../services/validation-user-role.service';
 
 @Component({
@@ -15,10 +16,12 @@ export class SidenavComponent implements OnInit {
   isAdmin: boolean = false;
   priceSelected: any;
   prices: any[] = [];
+  randomNumber: number = 0;
 
   constructor(private _router: Router,
     private _sharedService: SharedService,
-    private _validateRoleService: ValidateRoleService) {
+    private _validateRoleService: ValidateRoleService,
+    private _homeService: HomeService) {
     this.prices = [15, 20, 30];
   }
 
@@ -42,5 +45,10 @@ export class SidenavComponent implements OnInit {
     this._sharedService.removeItemFromLocalStorage('token');
     this._sharedService.removeItemFromLocalStorage('id');
   }
+
+  // generateRandomNumber(){
+  //   this.randomNumber = this._homeService.getRandomInt(1,75);
+  //   localStorage.setItem('RandomNumber', String(this.randomNumber));
+  // }
 
 }
--- frontend-bingo-velez/src/app/modules/home/pages/home/home.component.ts
+++ frontend-bingo-velez/src/app/modules/home/pages/home/home.component.ts
@@ -8,7 +8,6 @@ import { PlayersService } from 'src/app/core/services/players.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
   items: any;
   players: any;
   firstRow: any[] = [];
@@ -16,27 +15,55 @@ export class HomeComponent implements OnInit {
   thirdRow: any[] = [];
   fourthRow: any[] = [];
   fifthRow: any[] = [];
+  randomNumber: number = 0;
+  lastFiveNumbers: string = '';
+  numbers: number[] = [];
 
-  constructor(private _homeService: HomeService,
-    private _playerService: PlayersService) {}
+  constructor(
+    private _homeService: HomeService,
+    private _playerService: PlayersService
+  ) {}
 
   ngOnInit(): void {
-    var cartonesQuantity = localStorage.getItem("CantidadDeCartones");
-    this.items = this._homeService.items(cartonesQuantity ? Number(cartonesQuantity) : 30);
+    var cartonesQuantity = localStorage.getItem('CantidadDeCartones');
+    this.items = this._homeService.items(
+      cartonesQuantity ? Number(cartonesQuantity) : 30
+    );
 
-    this._playerService.getPlayers().subscribe(data => {
+    this._playerService.getPlayers().subscribe((data) => {
       this.players = data.list;
-    })
+    });
+
+    localStorage.removeItem('RandomNumber');
   }
 
-  resetCarton(carton: any){
-    var nuevoCarton = this._homeService.nuevoCarton(1);
+  resetCarton(carton: any) {
+    var newCarton = this._homeService.newCarton(1);
 
-    for(var key in nuevoCarton) {
-      var value = nuevoCarton[key];
+    for (var key in newCarton) {
+      var value = newCarton[key];
       value.index = carton.index;
       this.items[carton.index] = value;
     }
   }
 
+  setColorToCellNumber() {
+    this.randomNumber = this._homeService.getRandomInt(1, 75);
+    this.lastFivePlays(this.randomNumber);
+    localStorage.setItem('RandomNumber', String(this.randomNumber));
+    const newItems = this._homeService.searchThroughCartonesToSetNumber(
+      this.items
+    );
+    this.items = newItems;
+  }
+
+  lastFivePlays(play: number) {
+    if (this.numbers.length == 5) {
+      this.numbers.pop();
+      this.numbers.unshift(play);
+      this.numbers.join(' ');
+      return;
+    }
+    this.numbers.unshift(play);
+  }
 }
